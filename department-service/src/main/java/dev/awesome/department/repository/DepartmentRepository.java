3af892c9ea389c5d84590d3ef2d65fe6e5d89535
Rajan Vora <95693805+rajan-voradev@users.noreply.github.com>
2021-12-15 12:45:49 -0500

    Create DepartmentRepository.java

---
+++ department-service/src/main/java/dev/awesome/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package dev.awesome.department.repository;
+
+import dev.awesome.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
