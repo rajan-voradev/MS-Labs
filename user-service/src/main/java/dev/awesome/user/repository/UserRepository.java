cde3229c859576db07c692cf605cd00cf2059644
Rajan Vora <95693805+rajan-voradev@users.noreply.github.com>
2021-12-15 13:01:20 -0500

    Create UserRepository.java

---
+++ user-service/src/main/java/dev/awesome/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.awesome.user.repository;
+
+import dev.awesome.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUserId(Long userId);
+} 
