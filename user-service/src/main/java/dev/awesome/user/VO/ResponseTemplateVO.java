05071de3bccd8558c82acff7297fddd7906e603f
Rajan Vora <95693805+rajan-voradev@users.noreply.github.com>
2021-12-15 13:03:15 -0500

    Create ResponseTemplateVO.java

---
+++ user-service/src/main/java/dev/awesome/user/VO/ResponseTemplateVO.java
@@ -0,0 +1,14 @@
+package dev.awesome.user.VO;
+
+import dev.awesome.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseTemplateVO {
+    private User user;
+    private Department department;
+} 
